b1074262a818e13a91c092d45c51648779efa66b
stevenfermin <cesarstevenfermin@gmail.com>
2026-09-08 17:19:09 -0400

    adding notes

--- Week02/scripts/bom.js
+++ Week02/scripts/bom.js
@@ -2,8 +2,8 @@ let button = document.querySelector('button');
 let input = document.querySelector('#favchap');
 let list = document.querySelector('ul');
 
-const liList = document.createElement('li');
-const deletebutton = document.createElement('button');
+const liList = document.createElement('li'); //Creating a li element
+const deletebutton = document.createElement('button'); //Creating a delete button
 
 liList.textContent = input.value;
 deletebutton.textContent = '❌';
